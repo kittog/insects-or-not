ca45138ffbbdb5c791a2bddfc57bc0eb73276411
kittog <lena.gaubert@gmail.com>
2023-05-06 12:18:37 +0200

    ajout script xml parsing

---
+++ scripts/wiki_xml_parsing.py
@@ -0,0 +1,21 @@
+import sys
+import mwxml
+import glob
+
+if len(sys.argv) != 3:
+	print('La commande prend deux arguments : le chemin du fichier xml à lire, et le chemin du corpus txt à produire')
+	quit()
+infileName = sys.argv[1]
+outfileName = sys.argv[2]
+
+path = glob.glob(infileName)
+def process_dump(dump, path):
+    for page in dump:
+        for revision in page:
+            #yield page.title, revision.id, revision.timestamp, len(revision.text)
+            yield page.title, revision.text
+
+with open(outfileName, 'w') as f:
+    for page_title, rev_text in mwxml.map(process_dump, path):#paths
+        print("\n".join(str(v) for v in [page_title, rev_text]), file=f)
+        print("$$\n", file=f) # A modifier ou supprimer si vous souhaitez signaler autrement (ou ne pas signaler) la fin d'un article
